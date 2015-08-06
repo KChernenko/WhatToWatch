4ec7dcab72d90ffb689b2d37d3729d591017f88e
Constantine Chernenko <constantine.chernenko@gmail.com>
2015-08-07 02:02:38 +0300

    Fixed plot displaying bug

--- app/src/main/java/me/bitfrom/whattowatch/ui/activity/MainActivity.java
+++ app/src/main/java/me/bitfrom/whattowatch/ui/activity/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mCoordinator = (CoordinatorLayout) findViewById(R.id.main_container);
 
         if(savedInstanceState == null) {
