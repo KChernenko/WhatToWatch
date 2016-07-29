fdba4a70cc5444be03695e483266cbd21f7636a7
Constantine Chernenko <constantine.chernenko@gmail.com>
2016-07-29 15:34:51 +0300

    Cleanup deprecated code in the app intro

--- app/src/main/java/me/bitfrom/whattowatch/ui/activities/WWIntro.java
+++ app/src/main/java/me/bitfrom/whattowatch/ui/activities/WWIntro.java
@@ -2,16 +2,18 @@ package me.bitfrom.whattowatch.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
 import com.github.paolorotolo.appintro.AppIntro;
 
 import me.bitfrom.whattowatch.R;
 import me.bitfrom.whattowatch.ui.fragments.SliderFragment;
 
-public class WWIntro extends AppIntro{
+public class WWIntro extends AppIntro {
 
     @Override
-    public void init(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         showStatusBar(false);
         showSkipButton(false);
 
@@ -24,24 +26,15 @@ public class WWIntro extends AppIntro{
     }
 
     @Override
-    public void onSkipPressed() {
-
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
     }
 
     @Override
-    public void onNextPressed() {
-
-    }
-
-    @Override
-    public void onDonePressed() {
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
         Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
-    }
-
-    @Override
-    public void onSlideChanged() {
-
     }
 }
